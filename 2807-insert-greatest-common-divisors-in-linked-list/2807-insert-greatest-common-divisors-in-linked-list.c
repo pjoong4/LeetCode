149fd78bc56a11ec1f219573f3dbb94309b3fc10
pjoong4 <pjoong4@gmail.com>
2026-09-05 14:44:53 +0900

    Time: 89 ms (5.7%), Space: 15.5 MB (64.64%) - LeetHub

---
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* insertGreatestCommonDivisors(struct ListNode* head) {
+    struct ListNode* curr = head;
+
+    while(curr->next){
+        for(int i = curr->val ; i>=1 ; i--){
+            if((curr->next)&&(curr->next->val % i == 0) && (curr->val %i ==0)){
+                struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+                newNode->next = curr->next;
+                curr->next = newNode;
+                newNode->val = i;
+                printf("%d ", i);
+                curr =  newNode->next;
+                break;
+            }
+        }
+    }
+    return head;
+}
