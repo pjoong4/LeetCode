3cd4674487273be011293c8b8eb8b3a7344e39b3
pjoong4 <pjoong4@gmail.com>
2026-09-14 11:40:30 +0900

    Time: 0 ms (100%), Space: 8.7 MB (79.21%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.c
+++ 0191-number-of-1-bits/0191-number-of-1-bits.c
@@ -3,8 +3,9 @@ int hammingWeight(int n) {
    int result = 0;
 
     do{
-        if(n%2 == 1) result++;
-    }while(n/=2);
+        n&=(n-1);
+        result++;
+    }while(n);
 
     return result;
 }
